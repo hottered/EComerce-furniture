9a1fd84debef4a1a3f1b1c67947f23fdda298171
Aleksa <aleksa.mitrovic2001@gmail.com>
2023-09-04 20:35:52 +0200

    Init

---
+++ app/src/main/java/com/example/ecomerce/fragments/loginRegister/AccountOptionsFragment.kt
@@ -0,0 +1,7 @@
+package com.example.ecomerce.fragments.loginRegister
+
+import androidx.fragment.app.Fragment
+import com.example.ecomerce.R
+
+class AccountOptionsFragment : Fragment(R.layout.fragment_account_options) {
+}

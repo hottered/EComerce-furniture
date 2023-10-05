d17fe9e9d3a20559299519511a090a7ee8348320
Aleksa <aleksa.mitrovic2001@gmail.com>
2023-10-06 00:52:17 +0200

    CategoriesImplemented

--- app/src/main/java/com/example/ecomerce/fragments/categories/AccessoryFragment.kt
+++ app/src/main/java/com/example/ecomerce/fragments/categories/AccessoryFragment.kt
@@ -1,4 +1,76 @@
 package com.example.ecomerce.fragments.categories
 
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.example.ecomerce.data.Category
+import com.example.ecomerce.utill.Resource
+import com.example.ecomerce.viewmodel.CategoryViewModel
+import com.example.ecomerce.viewmodel.factory.BaseCategoryViewModelFactory
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.firestore.FirebaseFirestore
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import javax.inject.Inject
+
+@AndroidEntryPoint
 class AccessoryFragment : BaseCategoryFragment() {
+
+    @Inject
+    lateinit var firestore: FirebaseFirestore
+
+    val viewModel by viewModels<CategoryViewModel> {
+        BaseCategoryViewModelFactory(firestore, Category.Accessory)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.offerProducts.collectLatest {
+                when (it) {
+                    is Resource.Loading -> {
+                        showOfferLoading()
+                    }
+
+                    is Resource.Success -> {
+                        offerAdapter.differ.submitList(it.data)
+                        hideOfferLoading()
+                    }
+
+                    is Resource.Error -> {
+                        hideOfferLoading()
+                        Snackbar.make(requireView(), it.message.toString(), Snackbar.LENGTH_LONG)
+                            .show()
+                    }
+
+                    else -> Unit
+                }
+            }
+        }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.bestProducts.collectLatest {
+                when (it) {
+                    is Resource.Loading -> {
+                        showBestProductsLoading()
+                    }
+
+                    is Resource.Success -> {
+                        hideBestProductsLoading()
+                        bestProducts.differ.submitList(it.data)
+                    }
+
+                    is Resource.Error -> {
+                        hideBestProductsLoading()
+                        Snackbar.make(requireView(), it.message.toString(), Snackbar.LENGTH_LONG)
+                            .show()
+                    }
+
+                    else -> Unit
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/ecomerce/fragments/categories/BaseCategoryFragment.kt
+++ app/src/main/java/com/example/ecomerce/fragments/categories/BaseCategoryFragment.kt
@@ -5,9 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import androidx.core.widget.NestedScrollView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.ecomerce.R
 import com.example.ecomerce.adapters.BestProductAdapter
 import com.example.ecomerce.databinding.FragmentBaseCategoryBinding
@@ -15,8 +17,8 @@ import com.example.ecomerce.databinding.FragmentBaseCategoryBinding
 open class BaseCategoryFragment : Fragment(R.layout.fragment_base_category) {
 
     private lateinit var binding: FragmentBaseCategoryBinding
-    private lateinit var offerAdapter: BestProductAdapter
-    private lateinit var bestProducts: BestProductAdapter
+    protected val offerAdapter: BestProductAdapter by lazy {BestProductAdapter()}
+    protected val bestProducts: BestProductAdapter by lazy {BestProductAdapter()}
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,13 +31,49 @@ open class BaseCategoryFragment : Fragment(R.layout.fragment_base_category) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        
+
         setupOfferRv()
         setupBestProductsRv()
+        binding.rvOffer.addOnScrollListener(object : RecyclerView.OnScrollListener(){
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                
+                if(!recyclerView.canScrollVertically(1) && dx != 0) {
+                    onOfferPagingRequest()
+                }
+            }
+        })
+
+        binding.nestedScrollBaseCategory.setOnScrollChangeListener(NestedScrollView.OnScrollChangeListener{ v, _, scrollY, _, _ ->
+
+            if(v.getChildAt(0).bottom <= v.height + scrollY){
+                onBestProductsRequest()
+            }
+
+        })
     }
 
+    fun showOfferLoading(){
+        binding.offerProductsProgressBar.visibility = View.VISIBLE
+    }
+    fun hideOfferLoading(){
+        binding.offerProductsProgressBar.visibility = View.GONE
+    }
+    fun showBestProductsLoading(){
+        binding.bestProductsProgressbar.visibility = View.VISIBLE
+    }
+    fun hideBestProductsLoading(){
+        binding.bestProductsProgressbar.visibility = View.GONE
+    }
+    open fun onOfferPagingRequest(){
+        
+    }
+    open fun onBestProductsRequest(){
+        
+    }
+   
+
     private fun setupBestProductsRv() {
-        bestProducts = BestProductAdapter()
         binding.rvBestProducts.apply { 
             layoutManager = GridLayoutManager(requireContext(),2,GridLayoutManager.VERTICAL,false)
             adapter = bestProducts
@@ -43,7 +81,6 @@ open class BaseCategoryFragment : Fragment(R.layout.fragment_base_category) {
     }
 
     private fun setupOfferRv() {
-        offerAdapter = BestProductAdapter()
         binding.rvOffer.apply { 
             layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.HORIZONTAL,false)
             adapter = offerAdapter
